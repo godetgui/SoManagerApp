e794c5fb58e8cc0eb6ebca4d5e974555182f8797
etudiant <network>
2018-10-09 10:09:22 +0200

    Ajout IsMemberOf

--- app/src/main/java/fr/eseo/dis/godetgui/somanagerapp/data/isMemberOf.java
+++ app/src/main/java/fr/eseo/dis/godetgui/somanagerapp/data/isMemberOf.java
@@ -1,12 +1,41 @@
 package fr.eseo.dis.godetgui.somanagerapp.data;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
+import android.support.annotation.NonNull;
 
 @Entity(tableName = "roles", primaryKeys ={"idJM","idJurie"} ,
-        foreignKeys = {@ForeignKey(entity=JM.class, parentColumns = "id_film", childColumns = "id_film"),
-                @ForeignKey(entity = Artiste.class, parentColumns = "id_artiste",childColumns = "id_artiste")})
+        foreignKeys = {@ForeignKey(entity=JM.class, parentColumns = "idJM", childColumns = "idJM"),
+                @ForeignKey(entity = Juries.class, parentColumns = "idJurie",childColumns = "idJurie")})
 
 public class isMemberOf {
 
+    @NonNull
+    private int idJM;
+
+    @NonNull
+    private int idJurie;
+
+    public isMemberOf(int idJM, @NonNull int idJurie) {
+        this.idJM = idJM;
+        this.idJurie = idJurie;
+    }
+
+    public int getIdJM() {
+        return idJM;
+    }
+
+    public void setIdJM(int idJM) {
+        this.idJM = idJM;
+    }
+
+    @NonNull
+    public int getIdJurie() {
+        return idJurie;
+    }
+
+    public void setIdJurie(@NonNull int idJurie) {
+        this.idJurie = idJurie;
+    }
 }
